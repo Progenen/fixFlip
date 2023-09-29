5c9b5098c961d739c2729d66270d3013db375945
Stanislav Dombrovskiy <stanislav_2010@icloud.com>
2023-09-29 10:16:39 +0500

    Sdsa

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,3 +1,8 @@
+
+import noUiSlider from "nouislider";
+import Swiper from "swiper";
+import {Navigation} from "swiper/modules";
+
 document.addEventListener('DOMContentLoaded', function () {
     const burgerOpen = document.querySelector(".menu-open");
     const burgerClose = document.querySelector(".menu-close");
@@ -5,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalWrapper = document.querySelector(".modal-wrapper");
     const menu = document.querySelector(".mobile-menu");
     const header = document.querySelector("header");
-    const offer = document.querySelector(".offer");
     
     function insertAfter(newNode, existingNode) {
         console.log(existingNode.parentNode.children.length);
@@ -16,33 +20,52 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    burgerOpen.addEventListener("click", () => {
-        burger.classList.add("active");
-        modalWrapper.classList.add("active");
-        setTimeout(() => {
-            menu.classList.add("active");
-        }, 500)
-        document.body.classList.add("lock");
+    noUiSlider.create(document.querySelector("#calcFormRange"), {
+        start: [1000000],
+        connect: true,
+        tooltips: true,
+        range: {
+            'min': 1000000,
+            'max': 30000000
+        },
+        step: 1,
+        format: {
+            to: (value) => {
+                return value.toLocaleString('de-DE');
+            },
+            from: (value) => {
+                return value.replace('.', '');
+            }
+        }
+    });
 
-    })
+    // burgerOpen.addEventListener("click", () => {
+    //     burger.classList.add("active");
+    //     modalWrapper.classList.add("active");
+    //     setTimeout(() => {
+    //         menu.classList.add("active");
+    //     }, 500)
+    //     document.body.classList.add("lock");
 
-    burgerClose.addEventListener("click", () => {
-        burger.classList.remove("active");
-        modalWrapper.classList.remove("active");
-        setTimeout(() => {
-            menu.classList.remove("active");
-        }, 500)
-        document.body.classList.remove("lock");
-    })
+    // })
 
-    if (window.innerWidth <= 576) {
-        const headerPhone = document.querySelector(".header__phone");
-        const menuContacts = document.querySelector(".mobile-menu__contacts");
+    // burgerClose.addEventListener("click", () => {
+    //     burger.classList.remove("active");
+    //     modalWrapper.classList.remove("active");
+    //     setTimeout(() => {
+    //         menu.classList.remove("active");
+    //     }, 500)
+    //     document.body.classList.remove("lock");
+    // })
 
-        menuContacts.append(headerPhone);
-    }
+    // if (window.innerWidth <= 576) {
+    //     const headerPhone = document.querySelector(".header__phone");
+    //     const menuContacts = document.querySelector(".mobile-menu__contacts");
 
-    offer.style.paddingTop = header.clientHeight + "px";
+    //     menuContacts.append(headerPhone);
+    // }
+
+    // document.querySelector(".offer").style.paddingTop = header.clientHeight + "px";
 
     if (window.innerWidth <= 876) {
         const cardsSection =  document.createElement("section");
@@ -68,5 +91,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (window.innerWidth <= 620) {;
         document.querySelector(".offer .row").append(document.querySelector(".offer__dominates-item--1"))
+    }
+
+    if (document.querySelector(".footer__slider")) {
+        const footerSlider = new Swiper('.footer__slider', {
+            modules: [Navigation],
+            loop: true,
+            navigation: {
+                nextEl: ".footer__slider-arrow--right",
+                prevEl: ".footer__slider-arrow--left",
+            }
+        })
     }
 });
